4d849ce41e6a4b15389a5b35d46266491a9eb877
Maksius <103216048+Maksius@users.noreply.github.com>
2023-07-07 16:05:41 +0300

    count fix

--- hw2/script.js
+++ hw2/script.js
@@ -1,6 +1,6 @@
 let firstNumber = Number(prompt('Enter first number', ''));
 let lastNumber = Number(prompt('Enter second number', ''));
-let result;
+let result = 0;
 
 while(!Number.isInteger(firstNumber) || !Number.isInteger(lastNumber)) {
   alert('Enter integer number');
@@ -8,11 +8,23 @@ while(!Number.isInteger(firstNumber) || !Number.isInteger(lastNumber)) {
   lastNumber = Number(prompt('Enter second number', lastNumber));
 };
 
-let skip = confirm('Skip pair numbers?')
-if (skip===true && !(firstNumber%2===0) && !(lastNumber%2===0)) {
-  result = alert(firstNumber + lastNumber);
-} else if (skip===false) {
-  result = alert(firstNumber + lastNumber);
-} else {
-  result = alert(`Numbers: ', ${firstNumber}, ${lastNumber}`);
-};
+const skip = confirm('Skip pair numbers?')
+for (let i = firstNumber; i <= lastNumber; i++) {
+  if (skip) {
+      result += i % 2 ? i : 0;
+  } else {
+      result += i;
+  }
+}
+alert(`The result is: ${result}`)
+
+
+
+
+
+// if (skip===true && !(firstNumber%2===0) && !(lastNumber%2===0)) {
+//   result = alert(firstNumber + lastNumber);
+//   result += firstNumber % 2 ? firstNumber : 0;
+// } else {
+//   result += firstNumber;
+// };
